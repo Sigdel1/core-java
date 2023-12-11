e43343bb6e238b72aaec1eec4c9868342647ab9c
Maheshwar Sigdel <sigdelmaheshwar@gmail.com>
2023-12-11 09:39:44 -0500

    function basic

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,54 +2,52 @@ import javax.naming.Name;
 
 public class BankAccount {
 
- //accountHolderName-String-variable
+    //accountHolderName-String-variable
     String accountHolderName;
- //accountHolderAddress-String-variable
+    //accountHolderAddress-String-variable
     String accountHolderAddress;
- //accountHolderDateOfBirth-String-variable
+    //accountHolderDateOfBirth-String-variable
     String accountHolderDateOfBirth;
- //socialSecurityNumber-String-variable
+    //socialSecurityNumber-String-variable
     String socialSecurityNumber;
- //gender-String-variable
+    //gender-String-variable
     String gender;
- //accountType-String-variable
+    //accountType-String-variable
     String accountType;
- //accountBalance-double-variable
+    //accountBalance-double-variable
     double accountBalance;
 
- //accountNumber-String-variable
+    //accountNumber-String-variable
     String accountNumber;
- //routingNumber-String-variable
+    //routingNumber-String-variable
     String routingNumber;
- //phoneNumber-String-variable
+    //phoneNumber-String-variable
     String phoneNumber;
- //emailAddress-String-variable
+    //emailAddress-String-variable
     String emailAddress;
- //isMinorAccount-boolean-variable
+    //isMinorAccount-boolean-variable
     boolean isMinorAccount;
-  //bankName-String-constant(if single bank is take into consideration)
-   final String BANK_NAME = "Bank Of America";
+    //bankName-String-constant(if single bank is take into consideration)
+    final String BANK_NAME = "Bank Of America";
+
+
+    //Behavior
 
 
 
+    public static void main(String[] args) {
+        BankAccount tamanAccount = new BankAccount();
+        tamanAccount.accountHolderName="Taman Neupane";
+        tamanAccount.accountNumber="1233456";
 
 
+        BankAccount abcAccount = new BankAccount();
+        abcAccount.accountHolderName="ABC Neupane";
+        abcAccount.accountNumber="34568956";
+
+        System.out.println(tamanAccount.accountHolderName);
+        System.out.println(abcAccount.accountHolderName);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
---
+++ src/FunctionBasic.java
@@ -0,0 +1,32 @@
+public class FunctionBasic {
+    public static void main(String[] args) {
+        FunctionBasic fb = new FunctionBasic();
+
+        int a = 7;
+        int b = 3;
+        int result = fb.multiply(a, b);
+        int result1 = fb.add(a, b);
+        int result2= fb.subtract( a, b);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+
+
+
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+
+    }
+
+    public int add(int a, int b) {
+        return (a + b);
+    }
+    public int subtract(int a,int b) {
+        return (a -b);
+    }
+
+
+
+}
